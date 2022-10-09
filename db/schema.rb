451a5705dc4aab6542c26a744eef0bdaf42b8367
Cathella <ncathie214@gmail.com>
2022-10-09 16:27:15 +0300

    Add Comment model to Article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_08_071605) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_09_125615) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,4 +18,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_08_071605) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  add_foreign_key "comments", "articles"
 end
