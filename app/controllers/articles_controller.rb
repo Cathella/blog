84573f2ab2f0f2ef4edcf690224eaaf926cd0ced
Cathella <ncathie214@gmail.com>
2022-10-10 08:40:30 +0300

    Add basic authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: 'dhh', password: 'secret', expect: [:index, :show]
+
   def index
     @articles = Article.all
   end
